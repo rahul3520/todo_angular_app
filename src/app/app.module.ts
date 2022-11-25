3cb563e88f5f68e067a54d0c1aca6fbc6a393abb
rahulg3520 <rahul@nestgroup.net>
2022-11-25 12:35:05 +0530

    view all to do

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ViewallTodoComponent } from './viewall-todo/viewall-todo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    ViewallTodoComponent
   ],
   imports: [
     BrowserModule,
